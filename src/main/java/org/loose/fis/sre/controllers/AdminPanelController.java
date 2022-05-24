7fa3a9e99cb525bce9ab1dd33d7dfa879bda17fa
MarianG <marin.gataiant@gmail.com>
2022-05-24 23:47:11 +0300

    AF-7: Finished Admin Panel FXML

--- src/main/java/org/loose/fis/sre/controllers/AdminPanelController.java
+++ src/main/java/org/loose/fis/sre/controllers/AdminPanelController.java
@@ -2,27 +2,48 @@ package org.loose.fis.sre.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminPanelController {
     @FXML
     private Label titleLabel;
 
-    public void logOutOnAction(ActionEvent event){
-
+    public void logOutOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
     }
 
-    public void addOnAction(ActionEvent event){
-
+    public void addOnAction(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminAdd.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
     }
 
-    public void listOnAction(ActionEvent event){
-
+    public void listOnAction(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminList.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
     }
 
-    public void deleteOnAction(ActionEvent event){
-
+    public void deleteOnAction(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminDelete.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
     }
 
     public void closeButtonOnAction(ActionEvent event){
