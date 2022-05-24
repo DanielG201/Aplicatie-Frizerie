9b8c4d33fd30aea5679660fec8076fc44b87bddc
MarianG <marin.gataiant@gmail.com>
2022-05-25 00:19:56 +0300

    AF7: Finished FXML for registration add page

--- src/main/java/org/loose/fis/sre/models/Reservation.java
+++ src/main/java/org/loose/fis/sre/models/Reservation.java
@@ -4,13 +4,17 @@ public class Reservation {
 
     private String clientName,clientPhone,status,time,barberShopName,barberName;
 
-    public Reservation(String clientName,String clientPhone,String status,String time,String barberShopName,String barberName){
+    public Reservation(String clientName,String clientPhone,String time,String barberShopName,String barberName){
         this.clientName = clientName;
         this.clientPhone = clientPhone;
-        this.status = status;
+        this.status = "Liber";
         this.time = time;
         this.barberName = barberName;
         this.barberShopName = barberShopName;
+    }
+
+    public Reservation(){
+
     }
 
 }
