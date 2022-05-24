5d9961a8c34d4b2140abd6530f6207abddd982d7
MarianG <marin.gataiant@gmail.com>
2022-05-25 00:26:19 +0300

    AF7: Added some of the functions for adminAdd.fxml

--- src/main/java/org/loose/fis/sre/controllers/AdminAddController.java
+++ src/main/java/org/loose/fis/sre/controllers/AdminAddController.java
@@ -1,4 +1,46 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminAddController {
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private TextField nameField,phoneField,timeField,shopField,barberField;
+
+    public void addOnAction(ActionEvent event){
+        
+    }
+
+    public void logOutOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
+    }
+
+    public void closeButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) titleLabel.getScene().getWindow();
+        stage.close();
+    }
+
+    public void backOnAction(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminPanel.fxml"));
+        Stage myStage = (Stage) titleLabel.getScene().getWindow();
+        Scene scene = new Scene(root, 1080, 720);
+        myStage.setScene(scene);
+        myStage.show();
+    }
 }
